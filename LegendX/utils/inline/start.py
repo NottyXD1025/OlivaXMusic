34ddb0345d68a722f70119627bdf676786fd5e86
˹ʀσᴍᴇσ ꭙ˼ <109813393+RomeoX1025@users.noreply.github.com>
2023-02-18 12:58:59 +0530

    Update start.py

--- LegendX/utils/inline/start.py
+++ LegendX/utils/inline/start.py
@@ -37,6 +37,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         [
             InlineKeyboardButton(
                 text="❄️ ʜᴇʟᴩ ❄️", callback_data="settings_back_helper"
+            ),
             InlineKeyboardButton(
                 text="🍁 sᴜᴩᴩᴏʀᴛ 🍁", url=f"{config.SUPPORT_GROUP}"
             ),
